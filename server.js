7b029016483be80ddcace4e239d9ee454dc493d2
william07469 <clnkbrt127@gmail.com>
2026-03-21 20:12:43 +0300

    remove debug endpoint - system working

--- server.js
+++ server.js
@@ -404,17 +404,6 @@ app.get('/api/me', rateLimit, (req, res) => {
   });
 });
 
-// Debug endpoint to check environment variables
-app.get('/api/debug/env', (req, res) => {
-  res.json({ 
-    hasWebhookSecret: !!process.env.WHOP_WEBHOOK_SECRET,
-    webhookSecretLength: process.env.WHOP_WEBHOOK_SECRET?.length || 0,
-    nodeEnv: process.env.NODE_ENV || 'not set',
-    hasWhopApiKey: !!process.env.WHOP_API_KEY,
-    whopApiKeyLength: process.env.WHOP_API_KEY?.length || 0
-  });
-});
-
 // Plan bilgilerini döndür
 app.get('/api/plans', (req, res) => {
   res.json({
